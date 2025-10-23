d3daf09a44c69f7401784ba9be9083d29e1c58d3
yusrina112 <yusrinazh.lp3i.krw24@gmail.com>
2025-10-23 20:07:15 +0700

    keempat

--- lib/login_activity.dart
+++ lib/login_activity.dart
@@ -16,8 +16,20 @@ class LoginActivity extends StatelessWidget {
             SizedBox(height: 12),
             Text(
               "LKS MART",
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.black),
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.blueAccent),
             ),
+            
+            Text(
+              "Sign In",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+
+            Text(
+              "Enter your ID and password to sign in!",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, color: Colors.grey),
+              ),
+
+            Padding(padding: const EdgeInsets.all(8.0))
           ],
         ),
       ),
