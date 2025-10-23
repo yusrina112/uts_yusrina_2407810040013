26c75906b0f312e693a8f696b92a525d215e019f
yusrina112 <yusrinazh.lp3i.krw24@gmail.com>
2025-10-23 20:23:18 +0700

    kelima

--- lib/login_activity.dart
+++ lib/login_activity.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
-class LoginActivity extends StatelessWidget {
+class LoginActivity extends StatefulWidget {
   const LoginActivity({super.key});
 
+  @override
+  State<LoginActivity> createState() => _LoginActivityState();
+}
+
+class _LoginActivityState extends State<LoginActivity> {
+  bool _showpassword = true;
+  final TextEditingController _emailcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +36,25 @@ class LoginActivity extends StatelessWidget {
               style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, color: Colors.grey),
               ),
 
-            Padding(padding: const EdgeInsets.all(8.0))
+            Padding(padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 8,
+              children: [
+                Text("Email"),
+                TextField(
+                  controller: _emailcontroller,
+                  decoration: InputDecoration(
+                    hintText: "email",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8)
+                    ),
+                  ),
+                )
+              ],
+            ),
+            ),
           ],
         ),
       ),
